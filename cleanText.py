b8f01a87e5c5e6752c46ca79b80af325dd1c698f
yohannNDB <91455596+yohannNDB@users.noreply.github.com>
2023-11-16 15:22:14 +0100

    Update cleanText.py

--- cleanText.py
+++ cleanText.py
@@ -1,7 +1,13 @@
 def toLowerCAses(text):
-    for elt in
+    for elt in text:
+        if ord(elt)>=ord('A') and ord(elt)<ord('a'):
+            elt =chr(ord(elt)+26)
+        elif ord(elt)>ord(' ') and ord(elt)<ord('A'):
+            elt= ' '
+    pass
 def delete_punctation(text):
     pass
 
 def addTocleanedspeeches(text):
     pass
+
