dcd9aae4104c12eab5051d8901080cb154ad15f3
VictorL37 <victor.lassalle@efrei.net>
2023-11-16 16:21:37 +0100

    Update namePresidents(3).py

--- namePresidents(3).py
+++ namePresidents(3).py
@@ -1,9 +1,18 @@
+import os
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+
 def namepresident(files):
-    pass
+
 
 
 names = {"Chirac": "Jacques", "Giscard d'Estaing": "Valéry", "Hollande": "François",
-         "Macron": "Emmanuel", "Mitterand": "François"}
+         "Macron": "Emmanuel", "Mitterrand": "François","Sarkozy": "Nicolas"}
 
 
 def firstname(files):
